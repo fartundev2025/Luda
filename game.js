5c3dfe9178c92d707938d27fd4f9e2370e1c3eef
fartuun <fartuun@gmail.com>
2025-02-04 07:29:18 +0300

    added text

---
+++ game.js
@@ -0,0 +1,22 @@
+$(document).ready(() => {
+    $('.playBtn').click((e) => {
+        const number = Math.floor(Math.random() * 6) + 1;
+        $('#result').text(number);
+        $(e.currentTarget).prop('disabled', true);
+        
+        let message = '';
+        if (number === 6) {
+            message = "hamplyo Waad baastey!🎉";
+        } else if (number === 1) {
+            message = "sorry Waad dhacdey!😢";
+        }
+        
+        $('#winner').text(message);
+    });
+
+    $('#resetBtn').click(() => {
+        $('.playBtn').prop('disabled', false);
+        $('#result').text('-');
+        $('#winner').text('');
+    });
+});
